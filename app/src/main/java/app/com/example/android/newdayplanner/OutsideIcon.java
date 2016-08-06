9c67a7f295e5f98673626df7671dce3c586652c4
madisongoldman <cloudisnotfat@gmail.com>
2016-08-05 21:58:32 -0500

    added submit buttons

--- app/src/main/java/app/com/example/android/newdayplanner/OutsideIcon.java
+++ app/src/main/java/app/com/example/android/newdayplanner/OutsideIcon.java
@@ -4,44 +4,63 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class OutsideIcon extends AppCompatActivity {
+
+    private RadioGroup radioGroup;
+    private RadioButton radioButton;
+    private Button btn;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_outside_icon);
+
+        addListenerOnButton();
     }
 
-    public void onRadioButtonClicked(View view) {
-        //Has the button been selected??
-        boolean checked = ((RadioButton) view).isChecked();
+    public void addListenerOnButton() {
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        btn = (Button) findViewById(R.id.submitbutton);
 
-        //Check which one was clicked
-        switch(view.getId()) {
-            case R.id.yesbutton:
-                if (checked) {
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                // get selected radio button from radioGroup
+                int selectedId = radioGroup.getCheckedRadioButtonId();
+
+                // find the radiobutton by returned id
+                radioButton = (RadioButton) findViewById(selectedId);
+
+                RadioButton yesButton = (RadioButton) findViewById(R.id.yesbutton);
+                RadioButton noButton = (RadioButton) findViewById(R.id.nobutton);
+
+                if(selectedId == yesButton.getId()) {
                     String yes = "Great! If you're still feeling anxious, try some other methods from the Daily Reminders page";
                     displayMessage(yes);
                 }
-                break;
-
-            case R.id.nobutton:
-                if (checked) {
+                else if (selectedId == noButton.getId()) {
                     String no = "Even if it's not physical activity, thirty minutes outside every day is a great way to manage depression or anxiety! Try reading in a park, going for a run, or even just taking a walk around your neighborhood.";
                     displayMessage(no);
                 }
 
-                break;
-        }
+            }
+
+        });
+
+
     }
 
     private void displayMessage(String message) {
         TextView messageTextView = (TextView) findViewById(R.id.messageTextView);
         messageTextView.setText(message);
     }
-
-
 }
--- app/src/main/java/app/com/example/android/newdayplanner/ShowerIcon.java
+++ app/src/main/java/app/com/example/android/newdayplanner/ShowerIcon.java
@@ -4,38 +4,60 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ShowerIcon extends AppCompatActivity {
+
+    private RadioGroup radioGroup2;
+    private RadioButton radioButton2;
+    private Button btn2;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shower_icon);
+
+        addListenerOnButton();
     }
 
-    public void onRadioButtonClicked(View view) {
-        //Has the button been selected??
-        boolean checked = ((RadioButton) view).isChecked();
+    public void addListenerOnButton() {
+        radioGroup2 = (RadioGroup) findViewById(R.id.radioGroup);
+        btn2 = (Button) findViewById(R.id.submitbutton);
 
-        //Check which one was clicked
-        switch(view.getId()) {
-            case R.id.yesbutton:
-                if (checked) {
+        btn2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                // get selected radio button from radioGroup
+                int selectedId = radioGroup2.getCheckedRadioButtonId();
+
+                // find the radiobutton by returned id
+                radioButton2 = (RadioButton) findViewById(selectedId);
+
+                RadioButton yesButton = (RadioButton) findViewById(R.id.yesbutton);
+                RadioButton noButton = (RadioButton) findViewById(R.id.nobutton);
+
+                if(selectedId == yesButton.getId()) {
                     String yes = "Great! If you're still feeling anxious, try some other methods from the Daily Reminders page";
                     displayMessage(yes);
                 }
-                    break;
-
-            case R.id.nobutton:
-                if (checked) {
+                else if (selectedId == noButton.getId()) {
                     String no = "A shower is a good 'reset' button for the day. Try showering, and if you're still feeling anxious, try some other methods from the Daily Reminders page";
                     displayMessage(no);
                 }
 
-                    break;
-        }
+            }
+
+        });
+
+
     }
 
     private void displayMessage(String message) {
